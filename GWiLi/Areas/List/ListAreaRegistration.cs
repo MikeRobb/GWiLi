97699682630d9e09b927d193e7cd209aad93d7c7
MikeRobb <mike.robb@vikings.berry.edu>
2017-11-14 22:12:21 -0500

    Adding Areas Adding List Area

---
+++ GWiLi/Areas/List/ListAreaRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace GWiLi.Areas.List
+{
+    public class ListAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "List";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            // My List
+            context.MapRoute(
+                "MyList_default",
+                "MyList",
+                new { controller = "List", action = "GetAllMyLists", id = UrlParameter.Optional }
+            );
+            context.MapRoute(
+                "List_default",
+                "{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
