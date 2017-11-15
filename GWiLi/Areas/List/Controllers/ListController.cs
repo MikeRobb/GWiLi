496c5cd2429891d77419f378eee36c0f66ca3207
MikeRobb <mike.robb@vikings.berry.edu>
2017-11-14 22:14:06 -0500

    Adding list controller

---
+++ GWiLi/Areas/List/Controllers/ListController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GWiLi.Areas.List.Controllers
+{
+    public class ListController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAllMyLists()
+        {
+            return View("Index");
+        }
+    }
+}
